b0883f2f902791524a93e371d740d0b527c61b0f
LeoSmith <55242246+BetaBiest@users.noreply.github.com>
2020-05-26 15:56:26 +0200

    Update index.js
    deleted debug information

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function sketchchanger() {
     var choice = document.getElementById('sketch-selector').value;
-    console.log(choice);
     var link = 'sketches/' + choice;
-    console.log(link);
     document.getElementById('sketch-holder').setAttribute('src', link);
 }
