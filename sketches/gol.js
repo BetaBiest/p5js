742bc53b2be5b2808e0e2c548a9a5fc848324f42
LeoSmith <55242246+BetaBiest@users.noreply.github.com>
2020-05-26 18:47:24 +0200

    created ui for gol
    different interactions are now possible: klick to change the state of a single cell, play/pause, change the cellsize and animationspeed

--- sketches/gol.js
+++ sketches/gol.js
@@ -2,6 +2,19 @@ var grid;
 let size = {"cols": 0, "rows": 0};
 let scl = 10; //Boxsize in pixel
 let range = 1;
+let fr = 10;
+
+let paused = true;
+let selectedCellX = 0;
+let selectedCellY = 0;
+let pre_selectedCellX = 0;
+let pre_selectedCellY = 0;
+
+let input_container;
+let start_button;
+let size_slider;
+let fr_slider;
+
 
 
 function make2DArray(cols, rows) {
@@ -42,15 +55,49 @@ function countNeighbours(grid, x, y, cntRange = 1) {
     return sum;
 }
 
+function swap_pp() {
+    paused = !paused;
+    if (paused) this.html('Play');
+    else        this.html('Pause');
+}
 
+function mouseClicked() {
+    let x = floor(mouseX/scl);
+    let y = floor(mouseY/scl);
+    if (x <= size.cols && y <= size.rows) {
+        if (grid[x][y] == 1) grid[x][y] = 0;
+        else                 grid[x][y] = 1;
+    }
+}
 
 function setup() {
     makeCanvas();
 
-    background(100, 140, 210);
+    input_container = createDiv('');
+    input_container.id('input');
+
+    button = createButton('Play');
+    button.parent(input_container);
+    button.mousePressed(swap_pp);
+
+    size_slider = createSlider(5, 100, 10, 5);
+    size_slider.parent(input_container);
+
+    fr_slider = createSlider(5, 100, 60, 5);
+    fr_slider.parent(input_container);
+
+    
+    input_container.position(0, height - input_container.size().height - 10);
+
+    fr = fr_slider.value();
+    scl = size_slider.value();
+
+
+
+
+    frameRate(fr);
 
     size = calcNumOfCnR(size, scl);
-    
     grid = make2DArray(size.cols, size.rows);
 
     for (let x = 0; x < size.cols; x++){
@@ -59,6 +106,7 @@ function setup() {
         }
     }
     
+
 }
 
 function windowResized() {
@@ -66,15 +114,32 @@ function windowResized() {
     background(100, 140, 210);
     calcNumOfCnR(size, scl);
     grid = make2DArray(size.cols, size.rows);
-    for (let x in grid){
+    for (let x = 0; x < size.cols; x++){
         for (let y = 0; y < size.rows; y++) {
             grid[x][y] = floor(random(2));
         }
     }
+    input_container.position(0, height - input_container.size().height - 10);
 }
 
 function draw() {
     background(100, 140, 210);
+
+    if (scl != size_slider.value()) {
+        scl = size_slider.value();
+        size = calcNumOfCnR(size, scl);
+        grid = make2DArray(size.cols, size.rows);
+        for (let x = 0; x < size.cols; x++){
+            for (let y = 0; y < size.rows; y++) {
+                grid[x][y] = floor(random(2));
+            }
+        }
+    }
+
+    if (fr != fr_slider.value()) {
+        fr = fr_slider.value();
+        frameRate(fr);
+    }
 
     for (let x = 0; x < size.cols; x++) {
         for (let y = 0; y < size.rows; y++) {
@@ -87,23 +152,26 @@ function draw() {
         }
     }
 
-    let next = make2DArray(size.cols, size.rows);
 
-    // Compute next Gen
-    for (let x = 0; x < size.cols; x++) {
-        for (let y = 0; y < size.rows; y++) {
+    if (!paused) {
+        let next = make2DArray(size.cols, size.rows);
 
-            // Count neighbours in predefined range (1 == 3x3 field)
-            let neighbours = countNeighbours(grid, x, y, range);
+        // Compute next Gen
+        for (let x = 0; x < size.cols; x++) {
+            for (let y = 0; y < size.rows; y++) {
 
-            // Ruleset for GoL
-            let state = grid[x][y];
-            if (state == 0 && neighbours == 3) next[x][y] = 1;
-            else if (state == 1 && (neighbours < 2 || neighbours > 3)) next[x][y] = 0;
-            else next[x][y] = state;
+                // Count neighbours in predefined range (1 == 3x3 field)
+                let neighbours = countNeighbours(grid, x, y, range);
+
+                // Ruleset for GoL
+                let state = grid[x][y];
+                if (state == 0 && neighbours == 3) next[x][y] = 1;
+                else if (state == 1 && (neighbours < 2 || neighbours > 3)) next[x][y] = 0;
+                else next[x][y] = state;
+            }
         }
+
+        grid = next;
     }
 
-
-    grid = next;
 }
